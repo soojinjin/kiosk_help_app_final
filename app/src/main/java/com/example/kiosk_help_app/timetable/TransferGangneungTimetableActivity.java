796df9b1d9a36adfddc29fbb473a1fa545b098ec
Kei-Seo <ghdtjr3824@naver.com>
2021-12-04 18:46:33 +0900

    add transfer seat

--- app/src/main/java/com/example/kiosk_help_app/timetable/TransferGangneungTimetableActivity.java
+++ app/src/main/java/com/example/kiosk_help_app/timetable/TransferGangneungTimetableActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.kiosk_help_app.R;
+
 public class TransferGangneungTimetableActivity extends AppCompatActivity {
 
     @Override
@@ -13,7 +15,18 @@ public class TransferGangneungTimetableActivity extends AppCompatActivity {
         setContentView(com.example.kiosk_help_app.R.layout.activity_transfer_gangneung_timetable);
     }
 
-    public void GangneungOnClickHandler(View v){
+    public void GangneungOnClickHandler(View view){
+        switch (view.getId()) {
+          /*  case R.id.alone_transfer_2:
+                transaction.replace(com.example.kiosk_help_app.R.id.frameLayout, selectOneFragment).commitAllowingStateLoss();
+                break;
+            case R.id.alone_transfer_2:
+                transaction.replace(com.example.kiosk_help_app.R.id.frameLayout, selectTwoFragment).commitAllowingStateLoss();
+                break;
+            case R.id.alone_transfer_3:
+                transaction.replace(com.example.kiosk_help_app.R.id.frameLayout, selectThreeFragment).commitAllowingStateLoss();
+                break;*/
 
+        }
     }
 }
