c06813943ee3ffb6acda1e975164325d3469bed4
Kei-Seo <ghdtjr3824@naver.com>
2021-12-07 16:41:22 +0900

    add select seat in movie

--- app/src/main/java/com/example/kiosk_help_app/alone/Movie/AloneMovieOnstieActivity.java
+++ app/src/main/java/com/example/kiosk_help_app/alone/Movie/AloneMovieOnstieActivity.java
@@ -2,7 +2,11 @@ package com.example.kiosk_help_app.alone.Movie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.kiosk_help_app.R;
 
 public class AloneMovieOnstieActivity extends AppCompatActivity {
 
@@ -11,4 +15,25 @@ public class AloneMovieOnstieActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(com.example.kiosk_help_app.R.layout.activity_alone_movie_onstie);
     }
+
+    public void recommendMovieOnClick(View v){
+        switch (v.getId()){
+            case R.id.alone_recommend_movie_1:
+                Intent intent = new Intent(this, AloneMovieSeatSelect.class);
+                startActivity(intent);
+                break;
+
+            case R.id.alone_recommend_movie_2:
+                break;
+
+            case R.id.alone_recommend_movie_3:
+                break;
+
+
+
+        }
+    }
+
+
+
 }
