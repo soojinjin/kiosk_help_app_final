ec36ad0392ebc780f12f1ca8bb9efb9066820402
Kei-Seo <ghdtjr3824@naver.com>
2021-12-14 23:12:48 +0900

    add movie seat

--- app/src/main/java/com/example/kiosk_help_app/alone/Movie/AloneMovieSeatSelect.java
+++ app/src/main/java/com/example/kiosk_help_app/alone/Movie/AloneMovieSeatSelect.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 import com.example.kiosk_help_app.ListViewItem;
@@ -22,6 +24,14 @@ public class AloneMovieSeatSelect extends Activity {
         //타이틀바 없애기
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_alone_movie_seat_select);
+
+        ImageButton seat_1 = findViewById(R.id.alone_movie_seat_1);
+        seat_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //여기서는 좌석을 클릭했을 때 구현 내용이 들어가야한다.
+            }
+        });
 
     }
 
