bdc699385681f8e623663c1627bff3e5cc16c369
Kei-Seo <ghdtjr3824@naver.com>
2021-12-15 23:29:33 +0900

    movie xml update

--- app/src/main/java/com/example/kiosk_help_app/alone/Movie/AloneMovieSeatSelect.java
+++ app/src/main/java/com/example/kiosk_help_app/alone/Movie/AloneMovieSeatSelect.java
@@ -13,6 +13,7 @@ import android.widget.ListView;
 import com.example.kiosk_help_app.ListViewItem;
 import com.example.kiosk_help_app.ListviewAdapter;
 import com.example.kiosk_help_app.R;
+import com.example.kiosk_help_app.timetable.TransferReservationInfoActivity;
 
 import java.util.ArrayList;
 
@@ -24,15 +25,21 @@ public class AloneMovieSeatSelect extends Activity {
         //타이틀바 없애기
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_alone_movie_seat_select);
+        Intent intent = new Intent(this, AloneMovieSeatCheckAndPay.class);
+
 
         ImageButton seat_1 = findViewById(R.id.alone_movie_seat_1);
         seat_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //여기서는 좌석을 클릭했을 때 구현 내용이 들어가야한다.
+
             }
         });
 
     }
+
+
+
 
 }
